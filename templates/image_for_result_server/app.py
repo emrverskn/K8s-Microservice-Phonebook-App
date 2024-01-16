555d5e329c8eb9fac1754d1c9ea741bf40828778
Elif Merve Eriskin <140524629+emrverskn@users.noreply.github.com>
2024-01-16 15:30:07 +0300

    Updated app.py

--- templates/image_for_result_server/app.py
+++ templates/image_for_result_server/app.py
@@ -26,7 +26,6 @@ def find_persons(keyword):
     return persons
 
 
-# using template files named `index.html` given under `templates` folder
 # and assign to the static route of ('/')
 @app.route('/', methods=['GET', 'POST'])
 def find_records():
