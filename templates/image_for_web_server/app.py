e46059a3ca93e10cedd8d87646b37332f9decc09
Elif Merve Eriskin <140524629+emrverskn@users.noreply.github.com>
2024-01-27 18:46:01 +0300

    Update app.py

--- templates/image_for_web_server/app.py
+++ templates/image_for_web_server/app.py
@@ -63,7 +63,6 @@ def update_person(name, number):
 
     return f'Phone record of {name.strip().title()} is updated successfully'
 
-
 # and returns returns text info about result of the operation
 def delete_person(name):
     query = f"""
