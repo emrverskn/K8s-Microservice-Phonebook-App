234c86df91ae46d94549c0d268e1f979cfed3e83
Elif Merve Eriskin <140524629+emrverskn@users.noreply.github.com>
2024-01-29 14:14:21 +0300

    Update app.py

--- templates/image_for_web_server/app.py
+++ templates/image_for_web_server/app.py
@@ -80,7 +80,6 @@ def delete_person(name):
     return f'Phone record of {name.strip().title()} is deleted from the phonebook successfully'
 
 
-
 # using template files named `add-update.html` given under `templates` folder
 # and assign to the static route of ('add')
 @app.route('/add', methods=['GET', 'POST'])
