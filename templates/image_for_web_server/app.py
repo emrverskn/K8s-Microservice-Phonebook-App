228630ce2ca096ba6453a2b7eae0cfba7c58144b
Elif Merve Eriskin <140524629+emrverskn@users.noreply.github.com>
2024-01-30 18:03:37 +0300

    Update app.py

--- templates/image_for_web_server/app.py
+++ templates/image_for_web_server/app.py
@@ -80,7 +80,6 @@ def delete_person(name):
     return f'Phone record of {name.strip().title()} is deleted from the phonebook successfully'
 
 
-
 # and assign to the static route of ('add')
 @app.route('/add', methods=['GET', 'POST'])
 def add_record():
